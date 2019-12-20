391376b2efc7ed18c3bb98a1e585985540d0e8c3
sufeng <365490686@qq.com>
2019-12-20 19:41:56 +0800

    增加示例项目

---
+++ Demo/HelloMaven/src/main/java/com/sufeng/hello/Main.java
@@ -0,0 +1,10 @@
+package com.sufeng.hello;
+
+import org.joda.time.DateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        DateTime dateTime=new DateTime();
+        System.out.println(dateTime.getDayOfMonth());
+    }
+}
