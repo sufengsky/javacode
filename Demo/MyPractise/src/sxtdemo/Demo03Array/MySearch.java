d17b89826a0107ce869c9aa4543f367fe97c66fc
sufengsky <365490686@qq.com>
2019-12-23 23:58:31 +0800

    增加代码示例

--- Demo/MyPractise/src/sxtdemo/Demo03Array/MySearch.java
+++ Demo/MyPractise/src/sxtdemo/Demo03Array/MySearch.java
@@ -1,11 +1,13 @@
+package sxtdemo.Demo03Array;
+
 public class MySearch {
-	public class main(String[] args) {
-		int a[] = {2, 5, 99, 4, 6, 8, 7, 24, 12};
-		int m = 6;
-		for(int i=0; i<a.length; i++) {
-			if(a[i] == m) {
-				System.out.println(i);
-			}
-		}
-	}
+    public static void main(String[] args) {
+        int a[] = {2, 5, 99, 4, 6, 8, 7, 24, 12};
+        int m = 6;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == m) {
+                System.out.println(i);
+            }
+        }
+    }
 }
---
+++ Demo/MyPractise/src/sxtdemo/Demo09Thread/T.java
@@ -0,0 +1,18 @@
+package sxtdemo.Demo08Thread;
+
+public class T {
+    public static void main(String[] args) {
+        m1();
+    }
+
+    public static void m1() {
+        m2();
+        m3();
+    }
+
+    public static void m2() {
+    }
+
+    public static void m3() {
+    }
+}
