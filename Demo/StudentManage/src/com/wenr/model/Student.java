618be5f96ee58079a5a9368d718b9cda2955e6fa
sufeng <365490686@qq.com>
2020-01-17 21:53:56 +0800

    简单的学生选课系统

---
+++ Demo/StudentManage/src/com/wenr/model/Student.java
@@ -0,0 +1,26 @@
+package com.wenr.model;
+
+public class Student {
+	private int sid;		// 学号
+	private String spwd;	// 密码
+	private String sname;	// 姓名
+	
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getSpwd() {
+		return spwd;
+	}
+	public void setSpwd(String spwd) {
+		this.spwd = spwd;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+}
