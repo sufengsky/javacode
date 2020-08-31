1caff16d642bd84b96dc80a1381878b0662ec1f1
sufeng <365490686@qq.com>
2020-08-31 13:29:30 +0800

    格式调整

--- Demo/MyPractise/src/basic/demo0005/Main.java
+++ Demo/MyPractise/src/basic/demo0005/Main.java
@@ -6,13 +6,13 @@ public class Main {
 	 * 数据类型之间的转换
 	 */
 	public static void main(String[] args) {
-		String strNum="21";
-		
+		String strNum = "21";
+
 		//将数字字符串转换为整形数据
-		int num=Integer.parseInt(strNum);
-		
-		num=num-1;
-		
+		int num = Integer.parseInt(strNum);
+
+		num = num - 1;
+
 		System.out.println(num);
 	}
 
