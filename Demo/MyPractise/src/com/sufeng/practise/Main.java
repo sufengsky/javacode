2e757901b1f5de62d752f62335b72012e3fba69c
sufeng <365490686@qq.com>
2021-01-27 14:51:35 +0800

    增加HashMap与ArrayList的使用代码

--- Demo/MyPractise/src/com/sufeng/practise/Main.java
+++ Demo/MyPractise/src/com/sufeng/practise/Main.java
@@ -1,7 +1,48 @@
 package com.sufeng.practise;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Map<String, Person> map = new HashMap<>();
 
+        map.put("zs", new Person("zs", 18));
+        map.put("ls", new Person("ls", 28));
+
+        Person p = map.get("zs");
+        Person p2 = map.get("as");
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("zs", 18));
+        personList.add(new Person("ls", 28));
+        personList.sort((o1, o2) -> o1.getName().compareTo(o2.getName()));
+
+    }
+}
+
+
+class Person {
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 }
